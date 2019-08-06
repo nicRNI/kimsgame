e7fd462d59c85ec60f7a8f55af30add1b76cd7e7
nk00003 <nicolas.kochgallup@hsc.wvu.edu>
2019-08-06 17:31:21 -0400

    Finished POST test output; fixed click/blur issue in POST;

--- postJS.js
+++ postJS.js
@@ -21,6 +21,19 @@ function field_blur(field, ID)
 }
 
 
+
+//*******IF THEY RUN OUT OF TIME, MAKE IT AUTOMATICALLY SUBMIT, so that DATA is saved (maybe see how to press button w code?)
+
+var idnum2;
+function get_info()
+{
+    idnum2 = field.value;
+    localStorage["resp1"] = idnum2;
+
+}
+
+
+
 //Stop click event
 $('a').click(function(event){
     event.preventDefault();
