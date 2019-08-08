8be2c1ab720136330c97ec0f3b8c6f3ee146d47f
nk00003 <nicolas.kochgallup@hsc.wvu.edu>
2019-08-08 13:07:28 -0400

    Added 118 photos for index and tested them.

--- assets/imagedict.js
+++ assets/imagedict.js
@@ -1,38 +1,831 @@
 const o1 = {
-    img: 'assets/images/hat.png',
-    name: 'hat',
-    color: 'red'
+    img: 'assets/images/blackballoon.png',
+    name: 'balloon',
+    color: 'black',
+    photo: 'balloon1'
 };
 
+//make sure to 1. test file in imagetesting... 2. update in index preload... 3. update image array at bottom
+//update index selection amount
+
+
 const o2 = {
-    img: 'assets/images/hat2.png',
-    name: 'hat2',
-    color: 'green'
+    img: 'assets/images/blackboots.png',
+    name: 'boots',
+    color: 'black',
+    photo: 'boots1'
 };
 
 const o3 = {
-    img: 'assets/images/bike.png',
-    name: 'bike',
-    color: 'green'
+    img: 'assets/images/blackcat.png',
+    name: 'cat',
+    color: 'black',
+    photo: 'cat1'
 };
 
 const o4 = {
     img: 'assets/images/banana.png',
     name: 'banana',
-    color: 'blue'
+    color: 'blue',
+    photo: 'banana'
 };
 
 const o5 = {
-    img: 'assets/images/shirt.png',
-    name: 'shirt',
-    color: 'blue'
+    img: 'assets/images/blackdog.png',
+    name: 'dog',
+    color: 'black',
+    photo: 'dog1'
 };
 
 const o6 = {
-    img: 'assets/images/bluemittens.png',
-    name: 'bluemittens',
-    color: 'blue'
+    img: 'assets/images/blackdress.png',
+    name: 'dress',
+    color: 'black',
+    photo: 'dress1'
+};
+
+const o7 = {
+    img: 'assets/images/blackfork.png',
+    name: 'fork',
+    color: 'black',
+    photo: 'fork1'
+};
+
+const o8 = {
+    img: 'assets/images/blackglasses.png',
+    name: 'glasses',
+    color: 'black',
+    photo: 'glasses1'
+};
+
+const o9 = {
+    img: 'assets/images/blackglove.png',
+    name: 'glove',
+    color: 'black',
+    photo: 'glove1'
+};
+
+const o10 = {
+    img: 'assets/images/blacknotebook.png',
+    name: 'notebook',
+    color: 'black',
+    photo: 'notebook1'
+};
+
+const o11 = {
+    img: 'assets/images/blackpants.png',
+    name: 'pants',
+    color: 'black',
+    photo: 'pants1'
+};
+
+const o12 = {
+    img: 'assets/images/blackphone.png',
+    name: 'phone',
+    color: 'black',
+    photo: 'phone1'
+};
+
+const o13 = {
+    img: 'assets/images/blackspatula.png',
+    name: 'spatula',
+    color: 'black',
+    photo: 'spatula1'
+};
+
+const o14 = {
+    img: 'assets/images/bluebackpack.png',
+    name: 'backpack',
+    color: 'blue',
+    photo: 'backpack1'
+};
+
+const o15 = {
+    img: 'assets/images/bluebird.png',
+    name: 'bird',
+    color: 'blue',
+    photo: 'bird1'
+};
+
+const o16 = {
+    img: 'assets/images/bluebowtie.png',
+    name: 'bowtie',
+    color: 'blue',
+    photo: 'bowtie1'
+};
+
+const o17 = {
+    img: 'assets/images/bluedress.png',
+    name: 'dress',
+    color: 'blue',
+    photo: 'dress2'
+};
+
+const o18 = {
+    img: 'assets/images/bluefrog.png',
+    name: 'frog',
+    color: 'blue',
+    photo: 'frog1'
+};
+
+const o19 = {
+    img: 'assets/images/bluegloves.png',
+    name: 'gloves',
+    color: 'blue',
+    photo: 'gloves2'
+};
+
+const o20 = {
+    img: 'assets/images/bluehat.png',
+    name: 'hat',
+    color: 'blue',
+    photo: 'hat1'
+};
+
+const o21 = {
+    img: 'assets/images/bluejeans.png',
+    name: 'jeans',
+    color: 'blue',
+    photo: 'jeans1'
+};
+
+const o22 = {
+    img: 'assets/images/bluenecklace.png',
+    name: 'necklace',
+    color: 'blue',
+    photo: 'necklace1'
+};
+
+const o23 = {
+    img: 'assets/images/bluenotebook.png',
+    name: 'notebook',
+    color: 'blue',
+    photo: 'notebook2'
+};
+
+const o24 = {
+    img: 'assets/images/bluerubberduck.png',
+    name: 'rubberduck',
+    color: 'blue',
+    photo: 'duck1'
+};
+
+const o25 = {
+    img: 'assets/images/bluescarf.png',
+    name: 'scarf',
+    color: 'blue',
+    photo: 'scarf1'
+};
+
+const o26 = {
+    img: 'assets/images/bluetoothbrush.png',
+    name: 'toothbrush',
+    color: 'blue',
+    photo: 'toothbrush1'
+};
+
+const o27 = {
+    img: 'assets/images/bluetowel.png',
+    name: 'towel',
+    color: 'blue',
+    photo: 'towel1'
+};
+
+const o28 = {
+    img: 'assets/images/blueumbrella.png',
+    name: 'umbrella',
+    color: 'blue',
+    photo: 'umbrella1'
+};
+
+const o29 = {
+    img: 'assets/images/bluevase.png',
+    name: 'vase',
+    color: 'blue',
+    photo: 'vase1'
+};
+
+const o30 = {
+    img: 'assets/images/bluewaterbottle.png',
+    name: 'waterbottle',
+    color: 'blue',
+    photo: 'waterbottle1'
+};
+
+const o31 = {
+    img: 'assets/images/brownbelt.png',
+    name: 'belt',
+    color: 'brown',
+    photo: 'belt1'
+};
+
+const o32 = {
+    img: 'assets/images/brownbroom.png',
+    name: 'broom',
+    color: 'brown',
+    photo: 'broom1'
+};
+
+const o33 = {
+    img: 'assets/images/browncake.png',
+    name: 'cake',
+    color: 'brown',
+    photo: 'cake1'
+};
+
+const o34 = {
+    img: 'assets/images/browndog.png',
+    name: 'dog',
+    color: 'brown',
+    photo: 'dog2'
+};
+
+const o35 = {
+    img: 'assets/images/brownhorse.png',
+    name: 'horse',
+    color: 'brown',
+    photo: 'horse1'
+};
+
+const o36 = {
+    img: 'assets/images/brownrabbit.png',
+    name: 'rabbit',
+    color: 'brown',
+    photo: 'rabbit1'
+};
+
+const o37 = {
+    img: 'assets/images/brownshirt.png',
+    name: 'shirt',
+    color: 'brown',
+    photo: 'shirt1'
+};
+
+const o38 = {
+    img: 'assets/images/brownspoon.png',
+    name: 'spoon',
+    color: 'brown',
+    photo: 'spoon1'
+};
+
+const o39 = {
+    img: 'assets/images/browntable.png',
+    name: 'table',
+    color: 'brown',
+    photo: 'table1'
+};
+
+const o40 = {
+    img: 'assets/images/greensocks.png',
+    name: 'socks',
+    color: 'green',
+    photo: 'socks1'
+};
+
+const o41 = {
+    img: 'assets/images/greencar.png',
+    name: 'car',
+    color: 'green',
+    photo: 'car1'
+};
+
+const o42 = {
+    img: 'assets/images/greenflipflops.png',
+    name: 'flipflops',
+    color: 'green',
+    photo: 'flipflops1'
+};
+
+const o43 = {
+    img: 'assets/images/greenfrog.png',
+    name: 'frog',
+    color: 'green',
+    photo: 'frogg2'
+};
+
+const o44 = {
+    img: 'assets/images/greenhat.png',
+    name: 'hat',
+    color: 'green',
+    photo: 'hatg2'
+};
+
+const o45 = {
+    img: 'assets/images/greenleaf.png',
+    name: 'leaf',
+    color: 'green',
+    photo: 'leafg1'
+};
+
+const o46 = {
+    img: 'assets/images/greenlime.png',
+    name: 'lime',
+    color: 'green',
+    photo: 'lime1'
+};
+
+const o47 = {
+    img: 'assets/images/greenpencil.png',
+    name: 'pencil',
+    color: 'green',
+    photo: 'pencilg1'
+};
+
+const o48 = {
+    img: 'assets/images/greenshirt.png',
+    name: 'shirt',
+    color: 'green',
+    photo: 'shirtg2'
+};
+
+const o49 = {
+    img: 'assets/images/greenstapler.png',
+    name: 'stapler',
+    color: 'green',
+    photo: 'staplerg1'
+};
+
+const o50 = {
+    img: 'assets/images/greentoothbrush.png',
+    name: 'toothbrush',
+    color: 'green',
+    photo: 'toothbrushg2'
+};
+
+const o51 = {
+    img: 'assets/images/greentree.png',
+    name: 'tree',
+    color: 'green',
+    photo: 'treeg1'
+};
+
+const o52 = {
+    img: 'assets/images/greybackpack.png',
+    name: 'backpack',
+    color: 'grey',
+    photo: 'backpackg2'
+};
+
+const o53 = {
+    img: 'assets/images/greyrobe.png',
+    name: 'robe',
+    color: 'grey',
+    photo: 'robeg1'
+};
+
+const o54 = {
+    img: 'assets/images/greycouch.png',
+    name: 'couch',
+    color: 'grey',
+    photo: 'couchg1'
+};
+
+const o55 = {
+    img: 'assets/images/greyphone.png',
+    name: 'phone',
+    color: 'grey',
+    photo: 'phoneg2'
+};
+
+const o56 = {
+    img: 'assets/images/greyscarf.png',
+    name: 'scarf',
+    color: 'grey',
+    photo: 'scarfg3'
+};
+
+const o57 = {
+    img: 'assets/images/greysocks.png',
+    name: 'socks',
+    color: 'grey',
+    photo: 'socksg3'
+};
+
+const o58 = {
+    img: 'assets/images/orangebowl.png',
+    name: 'bowl',
+    color: 'orange',
+    photo: 'bowlo1'
+};
+
+const o59 = {
+    img: 'assets/images/orangecarrots.png',
+    name: 'carrots',
+    color: 'orange',
+    photo: 'carrotso1'
+};
+
+const o60 = {
+    img: 'assets/images/orangecat.png',
+    name: 'cat',
+    color: 'orange',
+    photo: 'cato2'
+};
+
+const o61 = {
+    img: 'assets/images/orangefish.png',
+    name: 'fish',
+    color: 'orange',
+    photo: 'fisho1'
+};
+
+const o62 = {
+    img: 'assets/images/orangehairbrush.png',
+    name: 'hairbrush',
+    color: 'orange',
+    photo: 'hairbrusho1'
+};
+
+const o63 = {
+    img: 'assets/images/orangeorange.png',
+    name: 'orange',
+    color: 'orange',
+    photo: 'orangeo1'
+};
+
+const o64 = {
+    img: 'assets/images/orangesocks.png',
+    name: 'socks',
+    color: 'orange',
+    photo: 'sockso3'
+};
+
+const o65 = {
+    img: 'assets/images/pinkchair.png',
+    name: 'chair',
+    color: 'pink',
+    photo: 'chairp1'
+};
+
+const o66 = {
+    img: 'assets/images/pinkhairbrush.png',
+    name: 'hairbrush',
+    color: 'pink',
+    photo: 'hairbrushp2'
+};
+
+const o67 = {
+    img: 'assets/images/pinkhat.png',
+    name: 'hat',
+    color: 'pink',
+    photo: 'hatp3'
+};
+
+const o68 = {
+    img: 'assets/images/pinknotebook.png',
+    name: 'notebook',
+    color: 'pink',
+    photo: 'notebookp3'
+};
+
+const o69 = {
+    img: 'assets/images/pinkscarf.png',
+    name: 'scarf',
+    color: 'pink',
+    photo: 'scarfp4'
+};
+
+const o70 = {
+    img: 'assets/images/pinkshoes.png',
+    name: 'shoes',
+    color: 'pink',
+    photo: 'shoesp2'
+};
+
+const o71 = {
+    img: 'assets/images/pinkstapler.png',
+    name: 'stapler',
+    color: 'pink',
+    photo: 'staplerp3'
+};
+
+const o72 = {
+    img: 'assets/images/pinksuitcase.png',
+    name: 'suitcase',
+    color: 'pink',
+    photo: 'suitcasep1'
+};
+
+const o73 = {
+    img: 'assets/images/pinkumbrella.png',
+    name: 'umbrella',
+    color: 'pink',
+    photo: 'umbrellap2'
+};
+
+const o74 = {
+    img: 'assets/images/purpleballoon.png',
+    name: 'balloon',
+    color: 'purple',
+    photo: 'balloonp3'
+};
+
+const o75 = {
+    img: 'assets/images/purplecar.png',
+    name: 'car',
+    color: 'purple',
+    photo: 'carp2'
+};
+const o76 = {
+    img: 'assets/images/purplechair.png',
+    name: 'chair',
+    color: 'purple',
+    photo: 'chairp2'
+};
+const o77 = {
+    img: 'assets/images/purplecomb.png',
+    name: 'comb',
+    color: 'purple',
+    photo: 'combp1'
+};
+const o78 = {
+    img: 'assets/images/purpleflower.png',
+    name: 'flowers',
+    color: 'purple',
+    photo: 'flowersp2'
+};
+const o79 = {
+    img: 'assets/images/purplehat.png',
+    name: 'hat',
+    color: 'purple',
+    photo: 'hatp4'
+};
+const o80 = {
+    img: 'assets/images/purplemug.png',
+    name: 'mug',
+    color: 'purple',
+    photo: 'mugp1'
+};
+const o81 = {
+    img: 'assets/images/purpletelephone.png',
+    name: 'phone',
+    color: 'purple',
+    photo: 'phonep4'
+};
+const o82 = {
+    img: 'assets/images/purpletoothbrush.png',
+    name: 'toothbrush',
+    color: 'purple',
+    photo: 'toothbrushp5'
+};
+
+const o83 = {
+    img: 'assets/images/redballoon.png',
+    name: 'balloon',
+    color: 'red',
+    photo: 'balloonr3'
+};
+
+const o84 = {
+    img: 'assets/images/redcherry.png',
+    name: 'cherry',
+    color: 'red',
+    photo: 'cherryr1'
+};
+
+const o85 = {
+    img: 'assets/images/redcup.png',
+    name: 'cup',
+    color: 'red',
+    photo: 'cupr1'
+};
+
+const o86 = {
+    img: 'assets/images/reddoor.png',
+    name: 'door',
+    color: 'red',
+    photo: 'doorr1'
+};
+
+const o87 = {
+    img: 'assets/images/redflag.png',
+    name: 'flag',
+    color: 'red',
+    photo: 'flagr1'
+};
+
+const o88 = {
+    img: 'assets/images/redflower.png',
+    name: 'flower',
+    color: 'red',
+    photo: 'flowerr3'
+};
+
+const o89 = {
+    img: 'assets/images/redglasses.png',
+    name: 'glasses',
+    color: 'red',
+    photo: 'glassesr3'
+};
+
+const o90 = {
+    img: 'assets/images/redmug.png',
+    name: 'mug',
+    color: 'red',
+    photo: 'mugr3'
+};
+
+const o91 = {
+    img: 'assets/images/redstapler.png',
+    name: 'stapler',
+    color: 'red',
+    photo: 'staplerr4'
+};
+
+const o92 = {
+    img: 'assets/images/redsweater.png',
+    name: 'sweater',
+    color: 'red',
+    photo: 'sweaterr2'
+};
+
+const o93 = {
+    img: 'assets/images/redphone.png',
+    name: 'phone',
+    color: 'red',
+    photo: 'phoner3'
+};
+
+const o94 = {
+    img: 'assets/images/redtie.png',
+    name: 'tie',
+    color: 'red',
+    photo: 'tier1'
 };
 
 
-window.imgArray = [o1, o1, o3, o2, o4, o1, o6, o4, o1, o5, o1, o2, o1, o1, o1, o1, o1, o1, o1]; //make sure 1 of each of the 100? pics
+const o95 = {
+    img: 'assets/images/redumbrella.png',
+    name: 'umbrella',
+    color: 'red',
+    photo: 'umbrellar3'
+};
+
+
+const o96 = {
+    img: 'assets/images/redwatermelon.png',
+    name: 'watermelon',
+    color: 'red',
+    photo: 'watermelonr1'
+};
+
+const o97 = {
+    img: 'assets/images/whitecar.png',
+    name: 'car',
+    color: 'white',
+    photo: 'carw3'
+};
+
+const o98 = {
+    img: 'assets/images/whitecat.png',
+    name: 'cat',
+    color: 'white',
+    photo: 'catw3'
+};
+
+const o99 = {
+    img: 'assets/images/whiteflower.png',
+    name: 'flower',
+    color: 'white',
+    photo: 'flowerw4'
+};
+
+const o100 = {
+    img: 'assets/images/whitehat.png',
+    name: 'hat',
+    color: 'white',
+    photo: 'hatw5'
+};
+
+const o101 = {
+    img: 'assets/images/whitehorse.png',
+    name: 'horse',
+    color: 'white',
+    photo: 'horsew2'
+};
+
+const o102 = {
+    img: 'assets/images/whitepants.png',
+    name: 'pants',
+    color: 'white',
+    photo: 'pantsw2'
+};
+
+const o103 = {
+    img: 'assets/images/whiteplate.png',
+    name: 'plate',
+    color: 'white',
+    photo: 'platew1'
+};
+
+const o104 = {
+    img: 'assets/images/whitesoap.png',
+    name: 'soap',
+    color: 'white',
+    photo: 'soapw1'
+};
+
+const o105 = {
+    img: 'assets/images/whitesocks.png',
+    name: 'socks',
+    color: 'white',
+    photo: 'socksw3'
+};
+
+const o106 = {
+    img: 'assets/images/yellowcar.png',
+    name: 'car',
+    color: 'yellow',
+    photo: 'cary4'
+};
+
+const o107 = {
+    img: 'assets/images/yellowcorn.png',
+    name: 'corn',
+    color: 'yellow',
+    photo: 'corny1'
+};
+
+const o108 = {
+    img: 'assets/images/yellowdress.png',
+    name: 'dress',
+    color: 'yellow',
+    photo: 'dressy3'
+};
+
+const o109 = {
+    img: 'assets/images/yellowjacket.png',
+    name: 'raincoat',
+    color: 'yellow',
+    photo: 'raincoaty1'
+};
+
+const o110 = {
+    img: 'assets/images/yellowleaf.png',
+    name: 'leaf',
+    color: 'yellow',
+    photo: 'leafy2'
+};
+
+const o111 = {
+    img: 'assets/images/yellowlemon.png',
+    name: 'lemon',
+    color: 'yellow',
+    photo: 'lemony1'
+};
+
+const o112 = {
+    img: 'assets/images/yellowlightbulb.png',
+    name: 'lightbulb',
+    color: 'yellow',
+    photo: 'lightbulby1'
+};
+
+const o113 = {
+    img: 'assets/images/yellownotebook.png',
+    name: 'notebook',
+    color: 'yellow',
+    photo: 'notebooky3'
+};
+
+const o114 = {
+    img: 'assets/images/yellowpillows.png',
+    name: 'pillows',
+    color: 'yellow',
+    photo: 'pillowsy1'
+};
+
+const o115 = {
+    img: 'assets/images/yellowrubberduck.png',
+    name: 'rubberduck',
+    color: 'yellow',
+    photo: 'ducky3'
+};
+
+const o116 = {
+    img: 'assets/images/yellowshirt.png',
+    name: 'shirt',
+    color: 'yellow',
+    photo: 'shirty5'
+};
+
+const o117 = {
+    img: 'assets/images/yellowsuitcase.png',
+    name: 'suitcase',
+    color: 'yellow',
+    photo: 'suitcasey1'
+};
+
+const o118 = {
+    img: 'assets/images/yellowumbrella.png',
+    name: 'umbrella',
+    color: 'yellow',
+    photo: 'umbrellay5'
+};
+
+
+
+
+
+
+window.imgArray = [o1,o2,o3,o4,o5,o6,o7,o8,o9,o10,o11,o12,o13,o14,o15,o16,o17,o18,o19,o20,o21,o22,o23,o24,o25,o26,o27,o28,o29,o30,o31,o32,o33,o34,o35,o36,o37,o38,o39,o40,o41,o42,o43,o44,o45,o46,o47,o48,o49,o50,o51,o52,o53,o54,o55,o56,o57,o58,o59,o60,o61,o62,o63,o64,o65,o66,o67,o68,o69,o70,o71,o72,o73,o74,o75,o76,o77,o78,o79,o80,o81,o82,o83,o84,o85,o86,o87,o88,o89,o90,o91,o92,o93,o94,o95,o96,o97,o98,o99,o100,o101,o102,o103,o104,o105,o106,o107,o108,o109,o110,o111,o112,o113,o114,o115,o116,o117,o118]; //make sure 1 of each of the 100? pics
